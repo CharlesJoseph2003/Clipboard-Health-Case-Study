c05615f9bc019ae0b0aaab4d0d9da7c6447d83be
CharlesJoseph2003 <105239968+CharlesJoseph2003@users.noreply.github.com>
2024-10-22 03:30:45 -0400

    removed unnesessary comments

--- data_analysis/operator_analysis.py
+++ data_analysis/operator_analysis.py
@@ -35,14 +35,3 @@ def operator_metrics_manager(df):
     return no_interest_count, engaged_count, deferred_count
 
 
-# no_interest_count, engaged_count, deferred_count = operator_metrics_owner(case_study)
-# print('Owner')
-# print(f"No Interest: {no_interest_count}")
-# print(f"Engaged: {engaged_count}")
-# print(f"Deferred to Other: {deferred_count}")
-
-# print('Manager')
-# no_interest_count, engaged_count, deferred_count = operator_metrics_manager(case_study)
-# print(f"No Interest: {no_interest_count}")
-# print(f"Engaged: {engaged_count}")
-# print(f"Deferred to Other: {deferred_count}")
